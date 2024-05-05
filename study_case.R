b37f1439f99f7316ad55edacbd68788966c6f414
mohamed1296 <48485131+mohamed1296@users.noreply.github.com>
2024-05-05 10:30:26 +0200

    correction of the errors,
    I corrected the errors that I got. Now the code returns reasonable results for all models (EV, EEV, ST, WS), however my computer can only handle a max of a approximately 1000 scenarios.

--- study_case.R
+++ study_case.R
@@ -28,46 +28,43 @@ print('Solution:')
 print(result1$objval)
 # 0
 print(result1$solution)
-write.csv(list(EVobj = result1$objval, EVsol = result1$solution), "EVsol.csv")
-
 # 1.03 0.84 1.15 2.01 1.28 2.40 1.22 0.87 0.00 0.00 0.00 0.00 0.00 0.00 0.00 0.00
 ####################################################################################
 #####################################################################################
 # Expected result problem EVV 
 x_bar = c(1.03, 0.84, 1.15, 2.01, 1.28, 2.40, 1.22, 0.87)
 x_bar_S = rep(x_bar, S)
-WS = diag(n*S) + rbind(rep(0,n*S) ,cbind( - 1 *diag(n*S-1), rep(0, n*S-1)))
-
+WS = diag(n*S) + rbind(rep(0,n*S), cbind( - 1 *diag(n*S-1), rep(0, n*S-1)))
+for (i in 1: S-1) {
+  WS[i*n + 1, i*n] = 0 
+}
 model2 = list()
 model2$modelsense <- "min"
 model2$obj = rep(1/S, n*S)
 model2$A = WS
 model2$rhs = c(omega) - x_bar_S
 model2$sense = rep(">=", n*S)
-
 result2 <- lp(model2$modelsense, model2$obj, model2$A, model2$sense, model2$rhs)
 
 
 print('Solution:')
 print(result2$objval)
 print(result2$solution)
-
-write.csv(list(EVVobj = result2$objval, EVVsol = result2$solution), "EVVsol.csv")
 #######################################################################################33
 ##########################################################################################
 #Recourse model 
 x_S = diag(n)
-for (i in 1: (S-1)) {
+for (i in 1:(S-1)) {
   x_S = rbind(x_S, diag(n))
 }
 
 
 model3 = list()
-model3$modelsense <- "min"
-model3$obj = cbind(rep(0,n), rep(1/S, n*S))
-model3$A =rbind(c(rep(1,n), rep(0,n*S)), cbind(x_S, WS))
+model3$modelsense = "min"
+model3$obj = c(rep(0,n), rep(1/S, n*S))
+model3$A = rbind(c(rep(1,n), rep(0,n*S)), cbind(x_S, WS))
 model3$rhs = c(M,c(omega)) 
-model3$sense = c ("<=", rep(">=", n*S))
+model3$sense = c("<=", rep(">=", n*S))
 
 result3 <- lp(model3$modelsense, model3$obj, model3$A, model3$sense, model3$rhs)
 
@@ -75,16 +72,19 @@ result3 <- lp(model3$modelsense, model3$obj, model3$A, model3$sense, model3$rhs)
 print('Solution:')
 print(result3$objval)
 print(result3$solution[1:n])
-write.csv(list(RMobj = result3$objval, RMsol = result3$solution), "RMsol.csv")
 ################################################################################################3
 ##########################################################################################3333
 # Wait and see model
+X_ws = matrix(0,S, n*S)
+for (i in 1:S) {
+  X_ws[i, ((i-1)*n +1) :(i*n)] = rep(1,n)
+}
 model4 = list()
 model4$modelsense <- "min"
-model4$obj = cbind(rep(0,n), rep(1/S, n*S))
-model4$A =rbind(c(rep(1,n), rep(0,n*S)), cbind(diag(n*S), WS))
-model4$rhs = c(omega) 
-model4$sense = c ("<=", rep(">=", n*S))
+model4$obj = cbind(rep(0,n*S), rep(1/S, n*S))
+model4$A =rbind(cbind(X_ws, matrix(0, S, n*S)), cbind(diag(n*S), WS))
+model4$rhs = c(rep(M, S),c(omega)) 
+model4$sense = c(rep("<=",S), rep(">=", n*S))
 
 result4 <- lp(model4$modelsense, model4$obj, model4$A, model4$sense, model4$rhs)
 
