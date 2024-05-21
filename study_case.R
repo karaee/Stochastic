b812f570f88723a67f10852af6525b71173e882d
Emre Kara <e.kara.boun@gmail.com>
2024-05-21 21:58:17 +0200

    TS sample size increased to 2000. Final Draft

--- study_case.R
+++ study_case.R
@@ -1,4 +1,5 @@
 library("lpSolve")
+set.seed(48643)
 # Parameters 
 n = 8
 S = 1000
@@ -10,89 +11,53 @@ W = diag(n) + rbind(rep(0,n) ,cbind( - 1 *diag(n-1), rep(0, n-1)))
 ############################################################################
 # Expected Value problem: EV################################################
 
-model1 = list()
-model1$modelsense <- "min"
-model1$obj = c(rep(0,n), rep(1,n))
-model1$A = rbind(c(rep(1,n), rep(0,n)), cbind(diag(n), W))
-model1$rhs = c(M, nu)
-model1$sense = c("<=", rep(">=", n))
+modelEV = list()
+modelEV$modelsense <- "min"
+modelEV$obj = c(rep(0,n), rep(1,n))
+modelEV$A = rbind(c(rep(1,n), rep(0,n)), cbind(diag(n), W))
+modelEV$rhs = c(M, nu)
+modelEV$sense = c("<=", rep(">=", n))
 
-write(a<- print(model1), "EVmod.txt")
-
-result1 <- lp(model1$modelsense, model1$obj, model1$A, model1$sense, model1$rhs)
-
+resultEV <- lp(modelEV$modelsense, modelEV$obj, modelEV$A, modelEV$sense, modelEV$rhs)
 
 print('Solution:')
-print(result1$objval)
+print(resultEV$objval)
 # 0
-print(result1$solution)
+print(resultEV$solution)
 
-write.csv(list(EVobj = result1$objval, EVsol = result1$solution), "EVsol.csv")
-# 1.03 0.84 1.15 2.01 1.28 2.40 1.22 0.87 0.00 0.00 0.00 0.00 0.00 0.00 0.00 0.00
 ####################################################################################
 #####################################################################################
 # Expected result EVV  ################################################
-x_EV = c(1.03, 0.84, 1.15, 2.01, 1.28, 2.40, 1.22, 0.87)
 
-set.seed(48643)
+# y_i(\omega) values are calculated by taking the maximum of two lower bounds on each y_i(\omega) (d_i formulas)
+# Faster approach for calculating the expected value
 
-rep = 50
+rep = 100
 S = 5000
-model2 = list()
-model2$modelsense <- "min"
-model2$obj = c(rep(1,n))
-model2$W = W
-model2$sense = rep(">=", n)
 
+x_EV = resultEV$solution[1:n]
 EEVs = c()
 for (it in 1:rep) { # Multiple Replications Procedure
-  print(it)
-  omega = matrix(rexp(S*n, rate = 1/nu),n)
-#  # Control input data
-#  rbind(nu= nu,
-#    means = apply(omega, 1, mean), # Check means
-#    sd = apply(omega, 1, sd)) # Check sd
-  
-  objVals2 <- c()
-  for (s in 1:S) {
-    model2$rhs = omega[,s] - x_EV
-    objVals2 <- c(objVals2,
-                  lp(model2$modelsense, model2$obj, model2$W, model2$sense, model2$rhs)$objval)
-  }
-  EEVs = c(EEVs, mean(objVals2))
-}
+   omegaEEV = matrix(rexp(S*n, rate = 1/nu),n)
+   objValsEEV <- c()
+   for (s in 1:S) {
+     d = max(omegaEEV[1,s] - x_EV[1], 0)
+     for (i in 2:n) {
+       d = c(d, max(omegaEEV[i,s] - x_EV[i] + d[i-1], 0) )
+     }
+     objValsEEV = c(objValsEEV, sum(d))
+   }
+   EEVs = c(EEVs, mean(objValsEEV))
+ }
 
-bestEEV = min(EEVs)
-cat("Mean: ", mean(EEVs) )
-cat("sd: ", sd(EEVs) )
-cat("Relative error: ", sd(EEVs)/mean(EEVs)*100, "%" )
-cat("Minimum EEV value: ", bestEEV)
-cat('EV: ', result1$objval)
+cat("Mean EEV Value: ", mean(EEVs) ,'\n')
+cat("CI on EEV: ", mean(EEVs) + sd(EEVs)*qt(0.975,rep-1)*c(-1,1) ,'\n')
+cat("Relative error: ", sd(EEVs)/mean(EEVs)*100, "%" ,'\n')
+cat('EV: ', resultEV$objval,'\n')
 
-write(paste(paste("Mean: ", mean(EEVs), "\n" ),
-            paste("sd: ", sd(EEVs) , "\n"),
-            paste("Relative error: ", sd(EEVs)/mean(EEVs)*100, "%" , "\n"),
-            paste("Minimum EEV value: ", bestEEV , "\n"),
-            paste('EV: ', result1$objval)),
-      "EEVresult.txt")
-######
-
-# model22 = list()
-# model22$modelsense <- "min"
-# model22$obj = rep(1/S, n*S)
-# model22$A = WS
-# model22$rhs = c(omega) - x_prop_S
-# model22$sense = rep(">=", n*S)
-# result22 <- lp(model22$modelsense, model22$obj, model22$A, model22$sense, model22$rhs)
-# 
-# 
-# print('Solution:')
-# print(result22$objval)
-#######################################################################################33
 ##########################################################################################
 #Recourse model ################################################
-set.seed(8733)
-S = 1000
+S = 2000
 x_S = diag(n)
 for (i in 1:(S-1)) {
   x_S = rbind(x_S, diag(n))
@@ -113,14 +78,11 @@ model3$sense = c("<=", rep(">=", n*S))
 
 result3 <- lp(model3$modelsense, model3$obj, model3$A, model3$sense, model3$rhs)
 
-
-print('Solution:')
-print(result3$objval)
-print(result3$solution[1:n])
-print(sum(result3$solution[1:n]))
+cat('TS Solution:',result3$objval,'\n')
+cat('x values:', result3$solution[1:n],'\n')
+cat('Sum of x values:', sum(result3$solution[1:n]),'\n')
 
 # Wait and see model ################################################
-set.seed(1327)
 S = 5000
 omegaWS = matrix(rexp(S*n, rate = 1/nu),n)
 
@@ -144,6 +106,7 @@ print(mean(objValsWS))
 
 ########
 ### question c ################################################
+S = 2000
 count = 0
 dum = result3$solution[(n+1):((n+1)*S)]
 for (i in seq(8, (n*S), by = 8)) {
@@ -151,37 +114,34 @@ for (i in seq(8, (n*S), by = 8)) {
     count = count + 1
   } 
 }
+cat('count', count, 'Prob', count/S,'\n')
 
 ################################################################################################
 #########################################################################################
-
-
 # question d################################################
 # Proportional Model ####
-set.seed(7867)
 x_prop = c(1.04, 0.85, 1.16, 2.03, 1.29, 2.42, 1.23, 0.88)
-omega3 = matrix(rexp(S*n, rate = 1/nu),n)
-objVals3 = c()
+omegaDprop = matrix(rexp(S*n, rate = 1/nu),n)
+objValsDprop = c()
 for (s in 1:S) {
-  d = max(omega3[1,s] - x_prop[1], 0)
+  d = max(omegaDprop[1,s] - x_prop[1], 0)
   for (i in 2:n) {
-    d = c(d, max(omega3[i,s] - x_prop[i] + d[i-1], 0) )
+    d = c(d, max(omegaDprop[i,s] - x_prop[i] + d[i-1], 0) )
   }
-  objVals3 = c(objVals3, sum(d))
+  objValsDprop = c(objValsDprop, sum(d))
 }
-cat("Average objective value for proportionality approach:", mean(objVals3))
+cat("Average objective value for proportionality approach:", mean(objValsDprop),'\n')
 
 # Expected value of TS Solution ####
 
-set.seed(1253)
 x_TS = result3$solution[1:n]
 omegaDts = matrix(rexp(S*n, rate = 1/nu),n)
-objVals3 = c()
+objValsDts = c()
 for (s in 1:S) {
   d = max(omegaDts[1,s] - x_TS[1], 0)
   for (i in 2:n) {
     d = c(d, max(omegaDts[i,s] - x_TS[i] + d[i-1], 0) )
   }
-  objVals3 = c(objVals3, sum(d))
+  objValsDts = c(objValsDts, sum(d))
 }
-cat("Average objective value for TS approach:", mean(objVals3))
+cat("Average objective value for TS approach:", mean(objValsDts),'\n')
